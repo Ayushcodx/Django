08aade7192fee935868f3bbdf06c93b352d80455
Ayushcodx <152431973+Ayushcodx@users.noreply.github.com>
2023-12-04 19:49:18 +0530

    Update asgi.py

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,10 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.apps import apps  # Add this import
-from django.conf import settings  # Add this import
+from room import routing  # Import the 'routing' module
 
 # Load the Django app configurations
+from django.apps import apps
+from django.conf import settings
 apps.populate(settings.INSTALLED_APPS)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
