76fe7fed4a33638371123bb5adaf93335737897f
Ayushcodx <152431973+Ayushcodx@users.noreply.github.com>
2023-12-04 19:21:29 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
@@ -102,8 +102,8 @@ DATABASES = {
     }
 }
 
-database_url=os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(str(databse_url))
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(str(database_url))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
