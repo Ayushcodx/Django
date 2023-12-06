5ea28f819afb54705b60b2414a2796c01d8d342d
Ayushcodx <152431973+Ayushcodx@users.noreply.github.com>
2023-12-06 15:46:47 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -20,8 +20,9 @@ from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
-from room.routing import websocket_urlpatterns
+from . import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
@@ -29,7 +30,9 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-        websocket_urlpatterns
+         [
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+]
         )
     )
 })
