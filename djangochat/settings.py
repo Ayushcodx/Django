34e1fbfa8ea7fa46b4a3bf822f89fc7b1971a52f
Ayushcodx <152431973+Ayushcodx@users.noreply.github.com>
2023-12-06 22:21:12 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -12,9 +12,80 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from decouple import config
 import dj_database_url
 from pathlib import Path
+#####################
+import json
 
+from django.contrib.auth import get_user_model
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
 
+from room.models import Room, Message
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        print(data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.save_message(username, room, message)
+
+        # Send message to chat 
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    # Receive message from chat 
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
+        }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        User = get_user_model() 
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
+#####################
 import os
+from django.urls import path
+
+
+
+websocket_urlpatterns = [
+    path('ws/<str:room_name>/', ChatConsumer.as_asgi()),
+]
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
