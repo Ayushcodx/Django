f66e2f6088ec1fcf5437710ff2b99b41cb068766
Ayushcodx <152431973+Ayushcodx@users.noreply.github.com>
2023-12-07 12:11:48 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -13,7 +13,22 @@ from decouple import config
 import dj_database_url
 from pathlib import Path
 
+"""
+WSGI config for djangochat project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
 import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+
+application = get_wsgi_application()
 
 
 
